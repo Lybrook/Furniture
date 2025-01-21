2bf03088e3f80d9b1b2f977311923aa732450e03
Lybrook <omoshlybrook@gmail.com>
2025-01-21 19:03:56 +0300

    update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import heroImage from 'src/assets/images/heroImage.jpg';
+import hero from '/src/assets/images/hero.jpg';
+
+
 
 const Hero = () => {
   const heroText = 'Crafting Beautiful Furniture and Interior Designs';
+  const heroButton = 'Explore Our Work';
 
   return (
     <section
       className="bg-cover bg-center h-screen"
-      style={{ backgroundImage: `url(${heroImage})` }}
+      style={{ backgroundImage: `url(${hero})` }}
     >
       <div className="flex justify-center items-center h-full bg-black bg-opacity-50 p-4">
         <h1 className="text-white text-4xl md:text-6xl font-bold text-center">{heroText}</h1>
-        <a href="#services" className="mt-6 inline-block bg-yellow-500 text-black py-2 px-6 rounded-lg hover:bg-yellow-600 transition">
-          Explore Our Work
+        <a
+          href="#services"
+          className="mt-6 inline-block bg-yellow-500 text-black py-2 px-6 rounded-lg hover:bg-yellow-600 transition"
+        >
+          {heroButton}
         </a>
       </div>
     </section>
