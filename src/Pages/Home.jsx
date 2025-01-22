c3f2dd32f510556c14c5dcb01026b917e0f7b130
Lybrook <omoshlybrook@gmail.com>
2025-01-22 10:40:17 +0300

    `Reformatted import statements and reordered components in Home.jsx`

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import Hero from '../Components/Hero';
-import About from '../Components/About';
-import Gallery from '../Components/Gallery';
-import Services from '../Components/Services';
-import Footer from '../Components/Footer';
-import RandomImage from '../Components/RandomImage';
-import ContactForm from '../Components/ContactForm';
+import React from "react";
+import Hero from "../Components/Hero";
+import About from "../Components/About";
+import Gallery from "../Components/Gallery";
+import Services from "../Components/Services";
+import Footer from "../Components/Footer";
+import RandomImage from "../Components/RandomImage";
+import ContactForm from "../Components/ContactForm";
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <Hero />
-      <About />
+    <div className='home-container'>
       <RandomImage />
+      <About />
+      <Hero />
       <Gallery />
       <Services />
       <ContactForm />
